d6f7d78dc508c7785082bd169207f0526f950afa
Yau1heni <lodbrok9@gmail.com>
2022-09-11 01:56:34 +0300

    07 component Technologies and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            Hello, samurai! Let's go!
-            <div>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JS</li>
-                    <li>React</li>
-                </ul>
-            </div>
+        <div>
+            <Header/>
+            <Technologies/>
         </div>
     );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>News</a>
+            <a href='#'>Messages</a>
+        </div>
+    )
 }
 
 export default App;
