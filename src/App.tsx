e3cb7d0acd422e6a8fce53871e7dcb0b1ee0f419
Yau1heni <lodbrok9@gmail.com>
2022-09-11 01:46:02 +0300

    06 index.js App JSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <div>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JS</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
