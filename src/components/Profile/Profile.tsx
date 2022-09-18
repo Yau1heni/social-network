de46e5fe5cd7b5b4e5e8125ef2081d77ebec17c1
Yau1heni <lodbrok9@gmail.com>
2022-09-18 23:12:12 +0300

    12 breakdown into components

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main className='content'>
+            <div>
+                <img src="" alt="img"/>
+            </div>
+            <div>ava</div>
+            <div>
+                <div>My posts</div>
+                <div>New posts</div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Profile;
