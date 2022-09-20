c6614d58ea268734d77a89c78df631dc476eff45
Yau1heni <lodbrok9@gmail.com>
2022-09-20 23:36:01 +0300

    21 dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_item}>
+                <div className={s.dialog}>Dimych</div>
+                <div className={s.dialog}>Zheka</div>
+                <div className={s.dialog}>Victor</div>
+                <div className={s.dialog}>Vasya</div>
+                <div className={s.dialog}>Anton</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo!</div>
+            </div>
         </div>
     );
 };
