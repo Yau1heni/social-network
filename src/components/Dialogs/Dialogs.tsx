5b1fa7a454211664c138b085fae0c32ceb92a4b0
Yau1heni <lodbrok9@gmail.com>
2022-09-23 17:51:21 +0300

    refactoring the structure of the dialog component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,40 +1,37 @@
 import React from 'react';
 import s from "./Dialogs.module.css"
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-const Message = (props: { message: string }) => {
-    return <div className={s.message}>{props.message}</div>
-}
-
-type DialogItemPropsType = {
-    name: string,
-    id: number
-}
-
-const DialogItem = (props: DialogItemPropsType) => {
-    return (
-        <div className={s.dialog}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
-        </div>
-    )
-}
 
 const Dialogs = () => {
+
+    let dialogs = [
+        {id: 1, name: 'Zheka'},
+        {id: 2, name: 'Kolyan'},
+        {id: 3, name: 'Toha'},
+        {id: 4, name: 'Igarusha'},
+        {id: 5, name: 'Max'},
+    ]
+
+    let messages = [
+        {message: 'Hi'},
+        {message: 'How are you?'},
+        {message: 'Hi'},
+        {message: 'Hi'},
+        {message: 'Hi'},
+    ]
+
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_item}>
-                <DialogItem name='Zheka' id={1}/>
-                <DialogItem name='Kolyan' id={2}/>
-                <DialogItem name='Toha' id={3}/>
-                <DialogItem name='Igarusha' id={4}/>
-                <DialogItem name='Max' id={5}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message='Hi'/>
-                <Message message='How are you?'/>
-                <Message message='Yo!'/>
-                <Message message='Yo!'/>
-                <Message message='Yo!'/>
+                {messagesElements}
             </div>
         </div>
     );
